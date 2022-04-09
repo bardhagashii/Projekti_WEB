c6fb398f8813a55d48b7ad25baa011f08d6a5f4c
bardhagashii <96657477+bardhagashii@users.noreply.github.com>
2022-04-10 00:20:47 +0200

    login.js
    validimi i login form me ane te javascript

---
+++ login.js
@@ -0,0 +1,36 @@
+var button = document.getElementById("submit");
+var usernameMsg =document.getElementById("usernameMsg");
+var passwordMsg = document.getElementById("passwordMsg");
+var usernameRegex = /[a-z]+[.]+[a-z]+/;
+var passwordRegex = /^[A-Z]\w+[a-z]\d{3}/
+
+button.addEventListener("click", function(event){
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if(username == "" || username == null){
+    usernameMsg.innerText="Please fill the username field!";
+    event.preventDefault();
+  }else{
+    if(usernameRegex.test(username)){
+        usernameMsg.innerText="";
+    }else{
+        usernameMsg.innerText="* Username must be example.example";
+        event.preventDefault();
+    }
+
+}
+
+  if (password == "" || password == null) {
+    passwordMsg.innerText = "* Please fill the password field";
+    event.preventDefault();
+  } else {
+    if (passwordRegex.test(password)) {
+      passwordMsg.innerText = "";
+    } else {
+      passwordMsg.innerText =
+        "*Password should start with a capital and end with 3 numbers";
+      event.preventDefault();
+    }
+  }
+});
