9906b62b6f3fa417cadcd18088bae92acf907055
bardhagashii <96657477+bardhagashii@users.noreply.github.com>
2022-04-15 23:08:55 +0200

    includes
    krijimi i pjeses header per cdo faqe ne folderin include

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+
+<div class="nav-area">
+    <div class="container">
+        <div class="logo">
+            <img src="img/bd.jpg" alt="logo">
+            <div class="links">
+                <a  href="index.php">Home</a>
+                <a  href="products.php">Products</a>
+                <a class="active" href="about.php">About</a>
+                <a href="contact.php">Contact</a>
+                <div class="LogIn">
+                    <a href="LogIn.php">Log In/Register</a>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
