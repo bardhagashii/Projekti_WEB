8d095cc92bb9f93d9a870cb772272c889a01f3c5
bardhagashii <96657477+bardhagashii@users.noreply.github.com>
2022-04-16 23:51:44 +0200

    Update register.js
    shtimi i disa kolonave ne formen register

--- register.js
+++ register.js
@@ -1,21 +1,50 @@
 var buttonn = document.getElementById("submitt");
 
+
+var fnameMsg = document.getElementById("fnameMsg");
+var lnameMsg = document.getElementById("lnameMsg");
 var userMsg = document.getElementById("userMsg");
 var emailMsg = document.getElementById("emailMsg");
 var pswMsg = document.getElementById("pswMsg");
 
-
-var userRegex = /^[A-Z]\w+[._-]?\w+/;
+var fnameRegex = /^[A-Z][a-z]{1,}/;
+var lnameRegex = /^[A-Z][a-z]{1,}/;
+var userRegex = /\w{5,14}/;
 var emailRegex = /[a-z]\w+@[a-z]+[-]?[a-z]\.[a-z]{2,3}/;
 var pswRegex = /^[A-Z]\w+[a-z]\d{3}/;
 
 buttonn.addEventListener("click", function(event){
+  var fname = document.getElementById("fname").value;
+  var lname = document.getElementById("lname").value;
   var user = document.getElementById("user").value;
   var email = document.getElementById("email").value;
   var psw = document.getElementById("psw").value;
 
+  if(fname == "" || fname == null){
+    fnameMsg.innerText="Please fill the first name field!";
+    event.preventDefault();
+  }else{
+    if(fnameRegex.test(fname)){
+        fnameMsg.innerText="";
+    }else{
+        fnameMsg.innerText="Name should start with a capital!"
+        event.preventDefault();
+    }
 
+}
   
+if(lname == "" || lname == null){
+  lnameMsg.innerText="Please fill the last name field!";
+  event.preventDefault();
+}else{
+  if(lnameRegex.test(lname)){
+      lnameMsg.innerText="";
+  }else{
+      lnameMsg.innerText="Last name should start with a capital!"
+      event.preventDefault();
+  }
+
+}
 
   if(user == "" || user == null){
     userMsg.innerText="Please fill the username field!";
@@ -24,13 +53,12 @@ buttonn.addEventListener("click", function(event){
     if(userRegex.test(user)){
         userMsg.innerText="";
     }else{
-        userMsg.innerText="* Please fill the username field correctly (name123)";
+        userMsg.innerText="Username must be between 5-15 characters!"
         event.preventDefault();
     }
 
 }
 
-  
 
 
 
@@ -41,7 +69,7 @@ if(email == "" || email == null){
   if(emailRegex.test(email)){
       emailMsg.innerText="";
   }else{
-      emailMsg.innerText="* Email must be standard";
+      emailMsg.innerText="Email must be standard!";
       event.preventDefault();
   }
 
@@ -54,12 +82,11 @@ if(psw == "" || psw == null){
   if(pswRegex.test(psw)){
       pswMsg.innerText="";
   }else{
-      pswMsg.innerText="* Password should start with a capital and end with 3 numbers";
+      pswMsg.innerText="Password should start with a capital and end with 3 numbers!";
       event.preventDefault();
   }
 
 }
-
 
 
 
