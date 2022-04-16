bd4b032c8ff9706e534ee98341f42469b0ab0d57
DardanKabashi <93317606+DardanKabashi@users.noreply.github.com>
2022-04-16 23:35:14 +0200

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<footer class="footer-bg">
+            <div class="container">
+        <div class="footer-content">
+            <a href="https://www.instagram.com/?hl=en" target="_blank"><i style="color: cornsilk;" class="fab fa-instagram fa-2x"></i></a>
+            <a href="https://www.facebook.com/" target="_blank"><i style="color: cornsilk;" class="fab fa-facebook fa-2x"></i></a>
+            <a href="https://twitter.com/?lang=en" target="_blank"><i style="color: cornsilk;" class="fab fa-twitter fa-2x"></i></a>
+        </div>
+        <div class="footer-content">
+            <p style="color: white; margin-top: 20px;">All Rights Reserved &copy; BestDecision, 2021</p>
+        </div>
+    </div>
+    </footer>
