58e43b8a54f7d963cd6758a34b64f91fa569174d
bardhagashii <96657477+bardhagashii@users.noreply.github.com>
2022-04-16 23:37:22 +0200

    news.php
    Krijimi i faqes se 5-te - News

---
+++ news.php
@@ -0,0 +1,30 @@
+<?php
+include 'includes/header.php';
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>About</title>
+
+
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+
+
+</head>
+
+<body>
+
+    <div class="news-banner"> 
+        <h1>Are you ready to make your Best Decision?</h1> <br>
+<h2>New products out! Hurry up and order yours now!<h2><br>
+</div>
+ 
+   <?php include 'includes/footer.php'; ?>
+
+
+</body>
+
+</html>
