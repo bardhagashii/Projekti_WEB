559f304e12f90452b7ece4d364abde4eaa370d61
bardhagashii <96657477+bardhagashii@users.noreply.github.com>
2022-04-24 23:22:26 +0200

    Update products.php

--- models/products.php
+++ models/products.php
@@ -1,51 +1,39 @@
 <?php
 
-	class User{
-		private $id;
-	    private $fname;
-	    private $lname;
-	    private $username;
-	    private $email;
-	    private $password;
-	    private $role;
+	class Product{
+		private $productID;
+		private $productName;
+		private $productText;
+		private $collection;
+		private $price;
 
-
-
-    	function __construct($id,$fname,$lname,$username,$email,$password,$role){
-			$this->id = $id;
-            $this->fname = $fname;
-            $this->lname = $lname;
-            $this->username = $username;
-            $this->email = $email;
-            $this->password = $password;
-            $this->role = $role;
-
-    	}
-		function getId(){
-			return $this->id;
+		function __construct($productID, $productName, $productText, $collection, $price){
+			$this->productID = $productID;
+			$this->productName = $productName;
+			$this->productText = $productText;
+			$this->collection = $collection;
+			$this->price = $price;
 		}
-    	function getFname(){
-       	   	return $this->fname;
+
+
+		function getProductID(){
+			return $this->productID;
+	 }
+		function getProductName(){
+       	   	return $this->productName;
    		}
 
-    	function getLname(){
-        	return $this->lname;
+    	function getProductText(){
+        	return $this->productText;
    		}
 
-   		function getUsername(){
-       		return $this->username;
+   		function getProductCollection(){
+       		return $this->collection;
    		}
 
-   		function getEmail(){
-       		return $this->email;
+   		function getProductPrice(){
+       		return $this->price;
    		}
 
-   		function getPassword(){
-      		return $this->password;
-   		}
-
-   		function getRole(){
-   			return $this->role;
-   		}
 	}
-?> 
+?>
