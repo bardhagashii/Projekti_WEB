d22ccfe311ebb631684c2580ee1e2c3aedc122c4
bardhagashii <96657477+bardhagashii@users.noreply.github.com>
2022-04-24 23:29:34 +0200

    Update news.php

--- news.php
+++ news.php
@@ -5,26 +5,170 @@ include 'includes/header.php';
 <html>
 
 <head>
-    <title>About</title>
-
-
+    <title>News</title>
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
-
-
 </head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {box-sizing: border-box}
+body {font-family: Verdana, sans-serif; margin:0}
+.mySlides {display: none}
+img {vertical-align: middle;}
 
+/* Slideshow container */
+.slideshow-container {
+  max-width: 1000px;
+  position: relative;
+  margin: auto;
+}
+
+/* butonat next dhe prev */
+.prev, .next {
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  width: auto;
+  padding: 16px;
+  margin-top: -22px;
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+  transition: 0.6s ease;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+  background-color: linen;
+}
+
+/* pozicionimi i butonit next djathtas */
+.next {
+  right: 0;
+  border-radius: 3px 0 0 3px;
+}
+
+/*ngjyra ne background */
+.prev:hover, .next:hover {
+  background-color: lightgreen;
+}
+
+/*teksti  */
+.text {
+  color: darkgreen;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+  font-weight: bold;
+}
+
+/* numrat (1/3 etc) */
+.numbertext {
+  color: black;
+  font-size: 14px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+  font-weight: bold;
+}
+
+/* pikat poshte slideve */
+.dot {
+  cursor: pointer;
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
+
+.active, .dot:hover {
+  background-color: darkolivegreen;
+}
+
+/* Fading animation */
+.fade {
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+
+@keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+/* ne ekrane me te vogla */
+@media only screen and (max-width: 300px) {
+  .prev, .next,.text {font-size: 11px}
+}
+</style>
+</head>
 <body>
 
-    <div class="news-banner"> 
-        <h1>Are you ready to make your Best Decision?</h1> <br>
-<h2>New products out! Hurry up and order yours now!<h2><br>
+<div class="slideshow-container">
+
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="img/new1.jpg" style="width:100%">
+  <div class="text">New Products!</div>
 </div>
- 
-   <?php include 'includes/footer.php'; ?>
 
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="img/new2.jpg" style="width:100%">
+  <div class="text">Hurry up and make your Best Decision!</div>
+</div>
 
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="img/new3.jpg" style="width:100%">
+  <div class="text">Order yours now!</div>
+</div>
+
+<a class="prev" onclick="plusSlides(-1)">❮</a>
+<a class="next" onclick="plusSlides(1)">❯</a>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+</div>
+
+<script>
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+</script>
+<?php include 'includes/footer.php'; ?>
 </body>
-
 </html>
